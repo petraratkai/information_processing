fb1585016ff25114245466067deb29add7e0d9c5
Razvan00Rusu <razvan.rusu19@imperial.ac.uk>
2021-03-11 19:51:45 +0000

    Initial Commit

---
+++ browser_server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+
+var app = express();
+var path = require('path');
+
+var response = {"is_running": false, "car1":0, "map": []};
+var timer;
+
+function mapInit(){
+    for(var i = 0; i < 20; i++){
+        response["map"][i] = Math.round(Math.random()*3);
+    }
+    timer = setInterval(mapUpdate, 1000);
+}
+
+function mapUpdate(){
+    for(var i = 379; i > -1; i--){
+        response["map"][i+20] = response["map"][i];
+    }
+    for(var i = 0; i < 20; i++){
+        response["map"][i] = Math.round(Math.random()*3);
+    }
+}
+
+mapInit();
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/getInfo', function(req, res) {
+    res.json(response);
+});
+
+app.get('/sendInfo', function(req, res) {
+    console.log("recieved request");
+    if(response["car1"] < 399){
+        response["car1"] += 1;
+    }
+    res.status(200).end();
+});
+
+app.get('/startGame', function(req, res) {
+    response['is_running'] = true;
+    res.status(200).end();
+});
+
+app.get('/endGame', function(req, res){
+    clearInterval(timer);
+    response['is_running'] = false;
+    res.status(200).end();
+});
+
+app.listen(3000);
