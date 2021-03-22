203cfc25d8ecb8d48aa5af9ae9649686135c7cbd
petraratkai <petraratkai18@imperial.ac.uk>
2021-03-22 23:40:48 +0000

    steering improved

--- car.js
+++ car.js
@@ -11,22 +11,32 @@ export default class Car {
   }
   update(x, y, width, height) {
     console.log(x + ' ' + y);
+    if(parseInt(x) > 70 && this.prev_xpos + 1 < width){
+      this.xpos = this.prev_xpos + 1;
+    }else if (parseInt(x) < -70 && this.prev_xpos - 1 >= 0){
+      this.xpos = this.prev_xpos - 1;
+    }
+    if(parseInt(y) > 70 && this.prev_ypos-1 >= 0){
+      this.ypos = this.prev_ypos - 1;
+    }else if (parseInt(y) < -70 && this.prev_ypos+1 < height){
+      this.ypos = this.prev_ypos + 1;
+    }
 
-    if(parseInt(x) + parseInt(this.prev_xpos) >= width-1){
+    /*if(parseInt(x) + parseInt(this.prev_xpos) >= width-1){
       this.xpos = width-1;
     }else if(parseInt(x) + parseInt(this.prev_xpos) <= 0){
       this.xpos = 0;
     }else{
       this.xpos = parseInt(x) + parseInt(this.prev_xpos);
     }
-    
+
     if(parseInt(y) + parseInt(this.prev_ypos) >= height-1){
       this.ypos = height-1;
     }else if(parseInt(y) + parseInt(this.prev_ypos) <= 0){
       this.ypos = 0;
     }else{
       this.ypos = parseInt(y) + parseInt(this.prev_ypos);
-    }
+    }*/
 
     this.prev_xpos = parseInt(this.xpos);
     this.prev_ypos = parseInt(this.ypos);
